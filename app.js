6919e1fc489e063dfee314dffd01fd13d3ca0594
Ishikagarg-ig <ishikagarg137@gmail.com>
2021-05-23 19:47:37 +0530

    created app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.sendFile('./views/index.html',{root:__dirname})
+})
+
+app.get('/add-item',(req,res)=>{
+    res.sendFile('./views/add-item.html',{root:__dirname})
+})
+
+app.use((req,res)=>{
+    res.sendFile('./views/error.html',{root:__dirname})
+})
+
+app.listen(3000);
